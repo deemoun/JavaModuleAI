5ff2cd12351f260c8b7a3ffa480873ed2dbeb694
Dmitry Y. <deemoun@users.noreply.github.com>
2024-01-25 14:59:59 +0100

    Additional code refactor

--- src/test/java/WebDriverTests.java
+++ src/test/java/WebDriverTests.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class WebDriverTests {
