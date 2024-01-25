ca926d9fafd1277c1f27ea98cfa9d7428359d6fb
Dmitry Y. <deemoun@users.noreply.github.com>
2024-01-25 15:22:11 +0100

    Empty Project for a lesson

--- src/test/java/UnitTests.java
+++ src/test/java/UnitTests.java
@@ -1,17 +1,4 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
-
 public class UnitTests {
-    // Write multiplication unit tests here
-    @Test
-    public void testMultiply() {
-        assertEquals(2, 1 * 1);
-    }
-    
-    // Write more multiplication unit tests here
-    @Test
-    public void testMultiply2() {
-        assertEquals(4, 2 * 2);
-    }
 }
