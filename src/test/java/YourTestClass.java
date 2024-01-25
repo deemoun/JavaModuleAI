2753a887eaed0c95c1cee579123bb98a640a86d8
Dmitry Y. <deemoun@users.noreply.github.com>
2024-01-25 14:44:29 +0100

    Main Test Code

--- src/test/java/YourTestClass.java
+++ src/test/java/YourTestClass.java
@@ -1,7 +1,10 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -19,8 +22,16 @@ public class YourTestClass {
     @Test
     void yourTest() {
         // Ваш тестовый код с использованием WebDriver
-        driver.get("https://www.example.com");
-        // Добавьте свой тестовый код здесь
+        driver.get("https://demoqa.com/automation-practice-form");
+        driver.findElement(By.id("firstName")).sendKeys("Dmitry");
+        driver.findElement(By.id("lastName")).sendKeys("Y");
+        driver.findElement(By.id("userNumber")).sendKeys("343434343434");
+
+        // Click a radio button using driver
+        WebElement element = driver.findElement(By.id("gender-radio-1"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+        WebElement element2 = driver.findElement(By.id("submit"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element2);
     }
 
     @AfterEach
